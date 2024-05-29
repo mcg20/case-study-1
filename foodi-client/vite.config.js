ad752cfef77ae388680b08825b6b36fbb4b00c24
Princess Mcglence Gamotia <princessmcglence.gamotia@g.msuiit.edu.ph>
2024-05-29 16:39:43 +0800

    first commit

--- foodi-client/vite.config.js
+++ foodi-client/vite.config.js
@@ -1,11 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensure this is pointing to the correct directory
+  root: '.', // Ensure this points to the correct directory
   build: {
     outDir: 'dist', // Ensure this matches your output directory
+    rollupOptions: {
+      input: 'index.html' // Ensure Vite knows where to find index.html
+    }
   },
 });
